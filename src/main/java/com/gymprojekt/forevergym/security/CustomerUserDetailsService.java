cb7ae04d8808f95c6cdb3a277e9a478f2be212c9
Zsombie-lol <zsombie@icloud.com>
2025-11-11 09:59:15 +0100

    A login és jwt token kész WEB-9 Service réteg

---
+++ src/main/java/com/gymprojekt/forevergym/security/CustomerUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.gymprojekt.forevergym.security;
+
+import com.gymprojekt.forevergym.exception.EmailNotFoundException;
+import com.gymprojekt.forevergym.model.User;
+import com.gymprojekt.forevergym.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomerUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws EmailNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new EmailNotFoundException("Felhasználó nem található: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
+}
